5ce2bd62b05417e0a4f5d6b39139cebea2fa51f8
Sneha <work.vellsneha@gmail.com>
2025-04-09 22:46:59 -0400

    added new changes

--- js/chatbot.js
+++ js/chatbot.js
@@ -8,19 +8,35 @@ class ChatBot extends HTMLElement {
             <img src="images/characterIcons/shiba-inu.png" alt="Bot Icon" class="bot-icon" />
             <p class="bot-message">Hi, do you need any help?</p>
         </span>
-        <button onclick="expandChat()">Yes</button>
+        <div class="chat-buttons">          
+          <button id="micBtn" aria-label="Microphone">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+              <path d="M12 14a3 3 0 003-3V5a3 3 0 10-6 0v6a3 3 0 003 3zm5-3a5 5 0 01-10 0H5a7 7 0 0014 0h-2z"/>
+            </svg>
+          </button>
+          <button id = "helpBtn" onclick="expandChat()">Yes</button>
+        </div>
+
       </div>
     
       <!-- Full chatbot -->
       <div class="chat-container" id="chatContainer">
         <div class="chat-header">
-          <h4>Chatbot</h4>
-          <button onclick="collapseChat()">❌</button>
+          <img src="images/characterIcons/shiba-inu.png" alt="Bot Icon" class="expanded-bot-icon" />
+          <h4>Eliot</h4>
+          <button onclick="collapseChat()" style = "color: black;">X</button>
         </div>
         <div class="chat-box" id="chatBox"></div>
         <div class="input-area">
           <input type="text" id="userInput" placeholder="Type your message..." />
-          <button onclick="sendMessage()">Send</button>
+          <div class="chat-buttons">          
+            <button id="micBtn" aria-label="Microphone">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                <path d="M12 14a3 3 0 003-3V5a3 3 0 10-6 0v6a3 3 0 003 3zm5-3a5 5 0 01-10 0H5a7 7 0 0014 0h-2z"/>
+              </svg>
+            </button>
+            <button id = "helpBtn" onclick="expandChat()">Send</button>
+          </div>
         </div>
       </div>
         `
